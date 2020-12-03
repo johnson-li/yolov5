702557bd3d031dae3657ea8f705c13dd5dcac4e9
Xuebing Li <lix16@5g-mobix.research.comnet.aalto.fi>
2020-12-03 23:21:05 +0200

    update

--- cmd/stream_local.sh
+++ cmd/stream_local.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/zsh
 source ~/.zshrc
-conda activate dev8
+conda activate dev
 
-concurrency=1 # max = 3
-exp_name=webrtc_exp5
+concurrency=3
+exp_name=webrtc_exp3
 weight=weights/yolov5x.pt
-ls /mnt/wd/$exp_name | xargs -P $concurrency -I FILE bash -c 'python -m stream_local -p /mnt/wd/'$exp_name'/FILE/dump -w '$weight
+ls ~/Data/$exp_name | xargs -P $concurrency -I FILE bash -c 'python -m stream_local -p ~/Data/'$exp_name'/FILE/dump -w '$weight
 
